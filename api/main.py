1bcb3b7df9672029f36d9b63ea7a30fe37b4ed38
sunRise9551 <wongkiesun@gmail.com>
2023-10-29 14:32:52 +0800

    API

--- api/main.py
+++ api/main.py
@@ -5,12 +5,27 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-endpoint = "http://localhost:8501/v1/models/potatoes_model:predict"
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+# endpoint = "http://localhost:8501/v1/models/potatoes_model:predict"
 MODEL = tf.keras.models.load_model("../models/2")
 CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
 
 @app.get("/ping")
 async def ping():
@@ -30,19 +45,20 @@ async def predict(
     image = read_file_as_image(await file.read())
     image_batch = np.expand_dims(image, 0)
 
-    json_data = {
-        "instances": image_batch.tolist()
-    }
+    predictions = MODEL.predict(image_batch)
 
-    response = requests.post(endpoint, json=json_data)
-    prediction = np.array(response.json()["predictions"][0])
-    # predictions = MODEL.predict(image_batch)
-    #
-    # predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
-    # confidence = np.max(predictions[0])
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
 
-    predicted_class = CLASS_NAMES[np.argmax(prediction)]
-    confidence = np.max(prediction)
+    # json_data = {
+    #     "instances": image_batch.tolist()
+    # }
+
+    # response = requests.post(endpoint, json=json_data)
+    # prediction = np.array(response.json()["predictions"][0])
+
+    # predicted_class = CLASS_NAMES[np.argmax(prediction)]
+    # confidence = np.max(prediction)
 
     return {
         'class': predicted_class,
